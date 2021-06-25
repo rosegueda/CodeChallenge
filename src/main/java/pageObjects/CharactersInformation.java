55da2cec30660c24ffbc32667009920c7c23a4f3
“rosegueda” <rosegueda@applaudostudios.com>
2021-06-25 14:16:11 -0600

    version6

---
+++ src/main/java/pageObjects/CharactersInformation.java
@@ -0,0 +1,26 @@
+package pageObjects;
+
+import java.util.List;
+
+public class CharactersInformation {
+    public Integer charId;
+    public String name;
+    public String birthday;
+    public List<String> occupation;
+    //public String occupation;
+    public String img;
+    public String status;
+    public String nickname;
+    public List<Integer> appearance;
+    //public List<String> appearance;
+    //public Integer appearance;
+    public String portrayed;
+    public String category;
+    public List<Object> betterCallSaulAppearance;
+//    public Object betterCallSaulAppearance;
+    //Examples:
+//    public String characters;
+//    public String episodes;
+//    public String quotes;
+//    public String deaths;
+}
